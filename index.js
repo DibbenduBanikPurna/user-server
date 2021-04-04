a29e0111eb116b74391abe5dc7018a3fd1a6330f
DibbenduBanikPurna <77313185+DibbenduBanikPurna@users.noreply.github.com>
2021-04-04 08:00:29 +0600

    my server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z2een.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+client.connect(err => {
+    const collection = client.db("pubg").collection("player");
+    app.post('/allMember',(req,res)=>{
+        const member=req.body
+        collection.insertMany(member)
+       
+    })
+        app.post('/newMember',(req,res)=>{
+            const add=req.body
+            collection.insertOne(add)
+        })
+    
+        app.get('/member',(req,res)=>{
+            collection.find({})
+            .toArray((err,documents)=>{
+                res.send(documents)
+            })
+    })
+
+
+    
+  });
+
+
+
+app.get('/',(req,res)=>{
+    res.send('fuck')
+})
+
+app.listen(5000,()=>console.log('5000 is calling'))
